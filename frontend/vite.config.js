b83d89d640af2d4828e672b889dd01797c9b7b2d
Abhishek kumar <rockabhisheksingh778189@gmail.com>
2025-03-16 00:30:14 +0530

    issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,22 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-})
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  },
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  }
+
+}
+);
